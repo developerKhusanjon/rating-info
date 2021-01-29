26a77734dd6b75f82c5482ccc69b579c1001c886
Khusanjon Tuychiboyev <developerkhusanjon@gmail.com>
2021-01-29 23:03:41 +0500

    init state

---
+++ src/main/java/io/developerkhusanjon/ratinginfo/RatingInfoApplication.java
@@ -0,0 +1,13 @@
+package io.developerkhusanjon.ratinginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/developerkhusanjon/ratinginfo/RatingInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.developerkhusanjon.ratinginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
